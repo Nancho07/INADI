fbd2edfe8a48425a07e764625eb6c523dbabf635
Nancho07 <hernan@imixdigital.com>
2017-11-09 15:25:02 -0600

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,33 +1,29 @@
-var socket;
-var usuario={};
-var alertas;
-var usuario={};
-usuario.prefix  = "inadi";
-        
+socket = io.connect("https://medic2-imixhn.c9users.io:8080");        
 function onAppReady() { 
     if( navigator.splashscreen && navigator.splashscreen.hide ) {   // Cordova API detected
         navigator.splashscreen.hide() ;
     }
-}
-document.addEventListener("app.Ready", onAppReady, false) ;
-$(document).ready(function() {
-    socket = io.connect("https://medic2-imixhn.c9users.io:8080/");
-    socket.on('connect', function () {
-        console.log("CONECTADO");
-        //socket.emit("saludo"); 
-        //$(".fa-address-book-o").css("color","");
-        //setName();
+    socket.on('connect', function(){
+        console.log("CONECTADO"); 
+        socket.emit("saludo"); 
     });
-
     $("#btn_acceso").on("click",function(){        
         document.location.href = "#acceso";
     });
     $(".btn_login").on("click",function(){
-        usuario.clave = MD5($("#clave").val());
+        usuario.pass = MD5($("#clave").val());
         usuario.usuario = $("#usuario").val();
-        usuario.pagina  ="loggin";
-        socket.emit('loggin',usuario);
-        
+        usuario.pagina  = "loggin"; 
+        console.log(usuario);
+        socket.emit("loggin",usuario);         
+    });
+    $("#usuario").keypress(function(){
+        var usuario = $("#usuario").val();
+        socket.emit('identify', usuario);
+    });
+    $("#usuario").blur(function(){
+        var usuario = $("#usuario").val();
+        socket.emit('identify', usuario);
     });
     $(".btn_menu").on("click",function(){ 
         document.location.href = "#menu";
@@ -37,7 +33,9 @@ $(document).ready(function() {
         //crearCookie("usuario_medicall",datos.name,0.020833333333,'/',"");
         document.location.href="#menu";
     });
-});
+}
+document.addEventListener("app.Ready", onAppReady, false) ;
+//$(document).ready(function() {});
 function setName() {
     //var datos ={};
     //usuario.usuario = getCookie('usuario_medicall');
